f4a9e54019c52196011955dc187a28c6b30c1d7b
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-06-08 19:41:16 +0000

    Creat function to get weight and height from the user and another function to validate the provided data by user is correct

--- run.py
+++ run.py
@@ -7,17 +7,36 @@ print("every person needs to know what category of BMI he is in\n")
 
 start = input("To start please type yes\n ")
 
-if start != "yes":
+if start.lower() != "yes":
     quit()
 
 
-def get_weightand_height():
+def get_weight_and_height():
     """
     Get the body weight and height from the user to calculate BMI
     """
     print("Great ! Please provide your body weight and height in the following format ")
     print("70, 175")
-    values_str = input("Enter the weight and height here: ")
-    print(f"Weight and height provided is {values_str}:") 
 
-get_weightand_height()
+    user_values_str = input("Enter the weight and height here:\n ")
+    user_data = user_values_str.split(", ")
+    validate_data(user_data) 
+
+
+def validate_data(values):
+    """
+    Converts all string values into integers, raises ValueError
+    if strings cannot be converted to integers and if data provided
+    is another format, all done by try statement
+    """
+
+    try:
+        if len(values) != 2:
+            raise ValueError(
+            f"exactly 2 value required, you provided {len(values)}"
+        )
+    except ValueError as e:
+        print(f"Provided Credentials are invalid: {e}, please try again\n ")
+
+
+get_weight_and_height()
