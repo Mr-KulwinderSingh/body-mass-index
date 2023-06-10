f169b96bb401ad8a4a13d74e7f58b4980a60469e
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-06-10 22:59:56 +0000

    Create validate data function to check the user input data is valid

--- run.py
+++ run.py
@@ -20,8 +20,27 @@ def get_user_data():
     print("Please provide your body weight(numbers only in kgs)")
     data1 = int(input(len("Enter weight here:\n ")))
     print("now enter your height(numbers only in cms)")
-    data2 = int(input("Enter the weight and height here:\n "))
+    data2 = int(input("Enter the height here:\n "))
     print(f"Your weight is : {data1}kgs and height is :{data2}cms ")
+
+    received_data = data1, data2
+    validate_data(received_data)
+
+
+def validate_data(values):
+    """
+    Checks if the given data is in number format, raises 
+    ValueError if there aren't 2 values
+    """
+    print(values)
+    try:
+        if len(values) != 2:
+            raise ValueError(
+                f"Only 2 values required: you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"provided data is incorrect: {e} please try again\n")
+
 
 
 
