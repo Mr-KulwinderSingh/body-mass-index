98bd84ba424de8c8639f568b4007c6d253df6b53
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-06-10 11:32:34 +0000

    Remove the validate data funtion and update input method in function get user data

--- run.py
+++ run.py
@@ -18,30 +18,12 @@ def get_user_data():
     
     print("Great !")
     print("Please provide your body weight(numbers only in kgs)")
-    data1_str = int(input("Enter weight here:\n "))
+    data1 = int(input(len("Enter weight here:\n ")))
     print("now enter your height(numbers only in cms)")
-    data2_str = int(input("Enter the weight and height here:\n "))
-    print(f"Weight and height provided are {data1_str, data2_str}")
-
-    data1_str=data2_str = user_data
-    validate_data(user_data) 
+    data2 = int(input("Enter the weight and height here:\n "))
+    print(f"Your weight is : {data1}kgs and height is :{data2}cms ")
 
 
-def validate_data(values):
-    """
-    Converts all string values into integers, raises ValueError
-    if strings cannot be converted to integers and if data provided
-    is another format, all done by try statement
-    """
 
-    try:
-        [int(value) for value in values]
-        if len(values) != 3:
-            print(f"Please enter valid numbers")
-    except ValueError as e:
-        print(f"Enter only numbers please: {e}, please try again.\n")
-
-
-  
 
 get_user_data()
