6a084e46f85aad446507b4350b9cd556db2e77bf
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-06-12 15:33:57 +0000

    Update calculate bmi function and it's if stament, plus add new input in get user data function

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import math
+
 print("Welcome to the Body_Mass_Index!\n")
 print("As per the modern life style, and living in the world of technology")
 print("every person needs to know what category of BMI he is in\n")
@@ -17,12 +19,13 @@ def get_user_data():
     """
     
     print("Great !")
-    print("Please provide your body weight(in kgs)")
     name = input("Please enter your name:\n")
+    print("Please provide your body weight(in kgs)")
+    
     data1 = int(input("Enter your weight here:\n "))
     print("now enter your height(only in cms)")
     data2 = int(input("Enter the height here:\n "))
-    print(f"Your weight is : {data1}kgs and height is :{data2}cms ")
+    print(f"Your weight is : {data1}kgs and height is :{data2}cms\n")
 
     calculate_bmi(name, data1, data2)
 
@@ -31,10 +34,13 @@ def get_user_data():
 def calculate_bmi(name, data1, data2):
     height = data2 / 100
     bmi = data1 / (height ** 2) 
-    if bmi < 25:
-        print(name + "You have a good body weight :)")
+    result = round(bmi,2)
+    print("Hey " + name +" "+ "your BMI is:", result)
+
+    if result < 25:
+        print( name + " " + "You have a good weight according to height:)\n")
     else:
-        print(name + "I'm sorry your wight is bit high")
+        print(name + " " + "I'm sorry your weight is a bit high\n")
 
     
 
