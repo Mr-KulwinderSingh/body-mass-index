7c6b1ad5b33dc505a7c5e8636f44467f6ea291eb
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-11-10 23:23:41 +0000

    Update the validate function for age and add new functions weight and height to individually test the user inputs, implement the while loop for user inputs to avoid empty data being entered and crash the app and deploy after corrections

--- run.py
+++ run.py
@@ -13,16 +13,16 @@ print(figlet_format("BODY MASS INDEX", font="small"))
 
 def get_user_data():
     """
-    This function starts the whole process of the BMI app with the  
-    welcome message & info about exception and rare statement for the  
+    This function starts the whole process of the BMI app with the
+    welcome message & info about exception and rare statement for the
     user. It appeals the user to follow the prompts. Get user data
     (name, age, body weight and height) from the user to calculate
     BMI. Runs a while loop to collect the valid data appropriate
     to the required age, the loop will be keep requesting until the
     given data is valid.
     """
-    
     print("            Welcome to the Body_Mass_Index!\n")
+    print("----------This BMI is for the age 16yrs to 100yrs ------------ ")
     print("--------------------------------------------------------------- ")
     print("Our modern eat-out style, and living in the world of technology")
     print("every person needs to know what category of BMI he/she is in\n")
@@ -31,46 +31,54 @@ def get_user_data():
     print("----------------------------------------------------------------")
     print("Adult women and men weighing 100kg who are between 201.0cm and")
     print("219.0 cm tall are considered to be of a healthy weight as measured")
-    print("by body mass index (bmi)2,but our BMI is restricted to 200cm height")
+    print("by body mass index (bmi),but our BMI is restricted to 200cm height")
     print("----------------------------------------------------------------")
     print("This BMI calculator has limits i.e. it can take the age of the ")
-    print("user 5 years to 100 years and weight 5kgs to 100kgs plus the height")
+    print(" 16 years to 100 years and weight 40kgs to 100kgs plus the height")
     print("restrictions upto 200cm, please consider them before entering your")
     print(".                   details! Good Luck! \n")
 
     start = input("To start using the BMI app please type yes:\n ")
 
     while start.lower() != "yes":
-        print(Fore.RED + f"In order to start the app, user has to type yes!\n ")
+        print(
+            Fore.RED + f"In order to start the app, user has to type yes!\n ")
         start = input("To start please type yes:\n ")
         print(f"You said {start} to start")
 
+    user1_name = input("Enter your name:\n")
+    while user1_name.strip() == "" or not user1_name.isalpha(
+    ) or (len(user1_name) <= 2) or (len(user1_name) >= 12):
+        print(
+            Fore.RED + "Username should be 3 to 12 characters only.")
+        user1_name = input("Enter your name:\n")
 
-
-
+    print(f'Hello {user1_name}')
     while True:
 
-        user1_name = input("Enter your Name:\n")
-        if user1_name.strip() == "":     
-            print(Fore.RED + " User name is required. Please enter a valid user name.")
-            continue
-        elif not user1_name.isalpha():  
-            print(Fore.RED + " User name should not contain numbers or symbols. Enter a valid user name.")
-            continue
         user1_age = input("Enter your Age (a whole number only):\n")
-        if not user1_age.isdigit():
-            print(Fore.RED + " User age is required. Please enter a valid age(a whole number).")
-            continue
-        user1_weight = input("Enter your Weight(in kgs only, do not enter decimal places):\n")
-        if not user1_weight.isdigit():
-            print(Fore.RED + " User weight is required. Please enter a valid weight(a whole number).")
-            continue
-        user1_height = input("Enter your Height(in cms only, do not enter decimal places):\n")
-        if not user1_height.isdigit():
-            print(Fore.RED + " User height is required. Please enter a valid height(a whole number).")
-            continue
-        else:
-            print("Data entered by the user:\n ")
+        while not user1_age.isdigit():
+            print(
+                Fore.RED +
+                " User age is required, enter a valid age(a whole number)")
+            user1_age = input("Enter your Age (a whole number only):\n")
+
+        user1_weight = input(
+            "Enter your Weight(in kgs only, decimal not allowed):\n")
+        while not user1_weight.isdigit():
+            print(
+                Fore.RED + "Weight is required, (a whole number only)")
+            user1_weight = input(
+                "Enter your Weight(in kgs only, decimal not allowed):\n")
+
+        user1_height = input(
+            "Enter your Height(in cms only, decimal not allowed):\n")
+        while not user1_height.isdigit():
+            print(
+                Fore.RED + "User height is required, (a whole number only)")
+            user1_height = input(
+                "Enter your Height(in cms only, decimal not allowed):\n")
+    # while True:
 
         class User:
             """
@@ -87,10 +95,10 @@ def get_user_data():
                 self.height = height
 
             def another(self):
-                print(Fore.YELLOW +f"Your name is {self.name}\n")
-                print(Fore.YELLOW +f"You are {self.user_age} years old\n")
-                print(Fore.YELLOW +f"Your weight is {self.weight}kgs\n")
-                print(Fore.YELLOW +f"Your height is {self.height}cms\n")
+                print(Fore.YELLOW + f"Your name is {self.name}\n")
+                print(Fore.YELLOW + f"You are {self.user_age} years old\n")
+                print(Fore.YELLOW + f"Your weight is {self.weight}kgs\n")
+                print(Fore.YELLOW + f"Your height is {self.height}cms\n")
 
         display_user = User(user1_name, user1_age, user1_weight, user1_height)
         display_user.another()
@@ -100,48 +108,34 @@ def get_user_data():
 
         calculate_bmi(user1_name, weight, height)
 
-        validate_username(user1_name)
         validate_user_weight(weight)
         validate_user_height(height)
 
         if validate_user_age(user_age):
             print("Thanks for using BMI Calculator App!\n ")
             break
+        elif validate_user_weight(weight):
+            print("Thanks for using BMI Calculator App!\n ")
+            break
+        elif validate_user_height(height):
+            print("Thanks for using BMI Calculator App!\n ")
+            break
     return user1_name, weight, height, user_age
-
-
-def validate_username(user1_name):
-    """
-    Inside the try, it converts all string values to integers,
-    raises the ValueError if string cannot be converted to
-    integers, or there aren't exact values as per requirement
-    """
-
-    try:      
-        
-        if (len(user1_name) <= 2) or (len(user1_name) >= 10):
-                raise ValueError(
-                    Fore.RED + f"User name should be min 3 or max 10 characters long"
-                )
-    except ValueError as e:
-        print(f"Invalid entry: {e}, please try again.\n ")
-        return False
-        
-    return True
+    print(user1_name, weight, height, user_age)
 
 
 def validate_user_age(user_age):
     """
     This function checks if the user enters an integer or not,
-    plus it also checks the error if the provided age range is 
+    plus it also checks the error if the provided age range is
     valid and if it's not valid value error will be triggered,
     any other exception will detected here and informed to the
-    user 
+    user
     """
     try:
-        if user_age < 5 or user_age > 100:
+        if user_age < 16 or user_age > 100:
             raise ValueError(
-                Fore.RED +f"Please correct user age range in order to calculate BMI"
+                Fore.RED + f"Correct user age in order to calculate BMI"
             )
     except ValueError as e:
         print(f"Invalid entry: {e}, please try again.\n ")
@@ -149,31 +143,32 @@ def validate_user_age(user_age):
 
     return True
 
+
 def validate_user_weight(weight):
     """
     Inside the try, this function checks if the user enters an integer
-    or not, plus it also checks an error if the provided weight range is 
-    valid & if it's not valid as per the BMI valid weight range,value error 
+    or not, plus it also checks an error if the provided weight range is
+    valid & if it's not valid as per the BMI valid weight range,value error
     will be triggered, empty or blank space can also be detected here
     in this validation function
     """
     try:
 
-        if weight < 10 or weight > 120:
+        if weight < 40 or weight > 120:
             raise ValueError(
-            Fore.RED +f"Sorry Invalid weight range entered: {weight}"
+                Fore.RED + f"Sorry Invalid weight range entered: {weight}"
             )
     except ValueError as e:
         print(f"Invalid entry: {e}, please try again.\n ")
         return False
-        
     return True
+
 
 def validate_user_height(height):
     """
     Inside the try, this function checks if the user enters an integer
-    or not, plus it also gives an error if the provided height range is 
-    valid and if it's not valid as per the BMI valid height range error 
+    or not, plus it also gives an error if the provided height range is
+    valid and if it's not valid as per the BMI valid height range error
     will be triggered, empty or blank space can also be detected here
     in in this validation function
     """
@@ -182,8 +177,8 @@ def validate_user_height(height):
 
         if height < 80 or height > 200:
             raise ValueError(
-                Fore.RED +f"Sorry Invalid height range entered:{height}"
-            ) 
+                Fore.RED + f"Sorry Invalid height range entered:{height}"
+            )
     except ValueError as e:
         print(f"Invalid entry:{e}, please try again.\n ")
         return False
